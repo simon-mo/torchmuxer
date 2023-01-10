ec23384fdae0fb84f3b4ea68836e7aaea7461774
simon-mo <simon.mo@hey.com>
2023-01-09 17:54:56 -0800

    Add activity api

--- src/fijit.h
+++ src/fijit.h
@@ -10,12 +10,15 @@ using namespace std;
 class Fijit {
 public:
   Fijit();
-  ~Fijit() = default;
+  ~Fijit();
 
   void run();
 
 private:
   CUcontext cudaCtx;
+
+  static void ActivtyAPIRequestBufferCallback(uint8_t **buffer, size_t *size, size_t *maxNumRecords);
+  static void ActivityAPICompleteBufferCallback(CUcontext ctx, uint32_t streamId, uint8_t *buffer, size_t size, size_t validSize);
 };
 
 #endif // FIJIT_SYS_FIJIT_H
